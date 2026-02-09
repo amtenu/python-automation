53ccb92f9b71389eaef4c57d8e35361feb12a7ba
amtenu <amannov21@gmail.com>
2026-02-09 15:03:14 -0700

    Check disk and cpu usage

---
+++ health_check.py
@@ -0,0 +1,11 @@
+import psutil
+import shutil
+
+def check_disk_usage(disk):
+    du=shutil.disk_usage(disk)
+    free_space=du.free/du.total*100
+    return free_space>20
+
+def check_cpu_usage():
+    usage=psutil.cpu_percent(1)
+    return usage <75
