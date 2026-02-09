a935213d677f8b2dab0c3b0ad08b66b4d2cc3692
amtenu <amannov21@gmail.com>
2026-02-09 15:09:39 -0700

    if one fails , send out alert

--- health_check.py
+++ health_check.py
@@ -9,3 +9,8 @@ def check_disk_usage(disk):
 def check_cpu_usage():
     usage=psutil.cpu_percent(1)
     return usage <75
+
+if not check_disk_usage("/") or not check_cpu_usage():
+    print("Error,Check disk or cpu ")
+else:
+    print("Everything is working properly!")
